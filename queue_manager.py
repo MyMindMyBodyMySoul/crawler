6fdc8fdf69ab888ca005028979438b7a84db6c50
MyMindMyBodyMySoul <falco.bechtle@online.de>
2015-11-28 12:43:17 +0100

    Added a functionality:
    queue_manager is now aware of how many times the
    host_queue has been cycled.
    
    The aim for this was to force the queue_manager to cycle through the current queue at least once and finish the current cycle before filling the host_queue with a new list.
    
    The idea works as follows:
    
    put_new_list() is called
    - check if queue has been cycled at least once and new cycle has not started
    - if true then call empty_queue, add new_list to the host_queue, reset "_times_fully_parsed" variable to 0 and
       "_new_list_holder" to none,
       else keep new_list in new_list_holder variable and return finishing the current cycle
    
    next_host() is called
    - check if _new_list_holder is none
       if true continue, else call put_new_list()
    - get first element
    - increment "_counter" variable
    - append current element at the end of host_queue
    - check if counter equals the length of the host_queue
     - if true reset "_counter" to 0 and increment "_times_fully_parsed" variable
        return current element

--- queue_manager.py
+++ queue_manager.py
@@ -38,6 +38,9 @@ class QueueManager(object):
         self._host_source_dict = {}
 
         self._counter = 0
+        self._new_list_holder = None
+        # Initial value of -1 is preventing getting trapped in the put_new_list function on startup
+        self._times_fully_parsed = -1
 
     def __call__(self, *args, **kwargs):
         return self
@@ -49,7 +52,11 @@ class QueueManager(object):
         :return str: hostname e.g. "google.com"
         """
         if not self._user_queue.empty():
+            # Trap for prioritizing the user_queue
             return self._user_queue.get()
+
+        if not self._new_list_holder is None:
+            self.put_new_list(self._new_list_holder)
 
         # cycle queue and keep track by counting
         hostname = self._host_queue.get()
@@ -59,6 +66,12 @@ class QueueManager(object):
 
         self._host_queue.put(hostname)
         self._counter += 1
+
+        # Keep track of how many times the host_queue has been "parsed" by sslyze
+        if self._counter == len(self._host_queue.queue):
+            self._counter = 0
+            self._times_fully_parsed +=1
+        print("The position counter:", self._counter," Times fully parsed: ",self._times_fully_parsed)
         return hostname
 
     def empty_queue(self):
@@ -69,7 +82,6 @@ class QueueManager(object):
                 *use this function with care*. it will empty the whole host_queue.
                 even if the host_queue has not finished yet.
         """
-        # two possibilities: emptying with a loop or garbage collection
         self._counter = 0
         self._host_source_dict = {}
         self._host_queue.put(None)
@@ -77,6 +89,7 @@ class QueueManager(object):
 
     def put_user_list(self, user_list):
         """
+        Feature on hold until further notice
 
         :param list user_list:
         :return:
@@ -90,17 +103,26 @@ class QueueManager(object):
 
         :param list new_list: nested list with hostname and source e.g. [ ["google.com", ["alexa-top-1m", ] ], ]
         """
-        # queue is not even through ?
-        if len(self._host_queue.queue) > self._counter:
+
+        # if no list has been supplied, leave method and continue
+        self._new_list_holder = new_list
+        if self._new_list_holder is None:
+            return
+
+
+        # Trap if sslyze is working the list for the fist time or is in the middle of working it the n-th time
+        if self._counter > 0 or self._times_fully_parsed == 0:
             return
 
         print("adding new list ...")
-        self.empty_queue()
+        self.empty_queue() # self_counter is reset to 0 in here
         for item in new_list:
             self._host_source_dict[item[0]] = item[1]
             self._host_queue.put(item[0])
 
         print("new list was added")
+        self._times_fully_parsed = 0
+        self._new_list_holder = None
 
 
     def next_result(self):
