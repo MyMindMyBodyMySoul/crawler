d4d8b1fb94ca1ee3693dd90fcbab73dfa26d7f4c
hasjan <jannik.haneu@gmail.com>
2015-11-30 17:36:33 +0100

    Added date-check and sleep function.
    Signed-off-by: hasjan <jannik.haneu@gmail.com>

--- input_worker.py
+++ input_worker.py
@@ -1,12 +1,14 @@
 from queue_manager import QueueClient
-import requests, zipfile, StringIO, shutil, time
+import requests, zipfile, StringIO, shutil, time, datetime
 
 __author__ = 'david'
 
 alexa_splits = [1000000, 1000, 100]
+nextDownloadDate = datetime.date.today()
 
 # creates list from AlexaCSV with dated sources
-def datedAlexaCSV(date, splits):
+def datedAlexaCSV(splits):
+    date = getAlexaCSV()
     splits.sort()
     m = splits[len(splits) - 1] + 1
     r = []
@@ -27,6 +29,7 @@ def datedAlexaCSV(date, splits):
 
 # downloads and unzips AlexaCSV and return the date
 def getAlexaCSV():
+    updateDownloadDate()
     #downloads from url
     r = requests.get("http://s3.amazonaws.com/alexa-static/top-1m.csv.zip")
     print("Finished downloading alexa top 1 million zip. Starting extraction...")
@@ -37,21 +40,37 @@ def getAlexaCSV():
     #moves file from "crawler" to "crawler/data"
     shutil.move("top-1m.csv", "data/top-1m.csv")
     #get date as "dd.mm.yyyy"
-    a = time.strftime("%d.%m.%Y")
-    return a
+    today = time.strftime("%d.%m.%Y")
+    return today
+
+def updateDownloadDate():
+    now = datetime.datetime.today()#get todays date
+    day = getattr(now, 'day')#get todays day
+    month = getattr(now, 'month')#get todays month
+    year = getattr(now, 'year')#get todays year
+    if day > 20:#day > 20 -> new list will be available on 20th next month
+        month = month + 1
+        if month > 12:
+            month = 1
+            year = year + 1
+    day = 20
+    nextDownloadDate = datetime.date(year,month,day)#creating a datetime.date object that tells, when to download a new list
 
 
 if __name__ == "__main__":
-
     # get instance of QueueClient
     c = QueueClient()
-
     # get appropriate queue from QueueClient
-    #host_queue = c.host_queue()
     queue_manager = c.queue_manager()
-    d = getAlexaCSV()
-    #host_queue.put(datedAlexaCSV(d, alexa_splits))
-    queue_manager.put_new_list(datedAlexaCSV(d, alexa_splits))
+    queue_manager.put_new_list(datedAlexaCSV(alexa_splits))
+    while(True):
+        t = datetime.datetime.today()#converting datetime.datetime.today object into datetime.date object
+        today = datetime.date(t.year,t.month,t.day)#converting datetime.datetime.today object into datetime.date object
+        if today > nextDownloadDate:
+            alexa_splits = [1000000, 1000, 100] #2. try crashes without this line at " m = splits[len(splits) - 1] + 1" ~ Line 13
+            queue_manager.put_new_list(datedAlexaCSV(alexa_splits))
+        print("sleeping now")
+        time.sleep(86400)#sleeps for 86400 seconds, ~1 day
 
 
 
