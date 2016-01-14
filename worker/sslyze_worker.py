307413a0286dfb8ec90b0a5a9b067ec03f926057
hoeyng <davidhoeyng@gmail.com>
2016-01-14 18:41:45 +0100

    fix memory bug

--- worker/sslyze_worker.py
+++ worker/sslyze_worker.py
@@ -14,7 +14,7 @@ from server.queue_manager import QueueClient
 from sslyze.plugins import PluginsFinder
 
 try:
-    from utils.ServersConnectivityTester import ServersConnectivityTester, InvalidTargetError
+    from sslyze.utils.ServersConnectivityTester import ServersConnectivityTester, InvalidTargetError
 except ImportError as e:
     print str(e) + '\nERROR: Could not import nassl Python module. Did you clone SSLyze\'s repo ? \n' +\
     'Please download the right pre-compiled package as described in the README.\n'
@@ -28,61 +28,59 @@ class WorkerProcess(Process):
     """
     This class checks hosts from the host_queue with the settings specified in settings.py.
     """
-    def __init__(self, qm, available_commands, i):
+    def __init__(self, qm, available_commands):
         Process.__init__(self)
 
         self._qm = qm
         self.available_commands = available_commands
-        self._i = i
 
     def run(self):
         # Plugin classes are unpickled by the multiprocessing module
         # without state info. Need to assign shared_settings here
-        from plugins.PluginBase import PluginResult
+
         for plugin_class in self.available_commands.itervalues():
             plugin_class._shared_settings = settings.SHARED_SETTINGS
 
-        while True:
+        hostname, user_id = self._qm.next_host()
 
-            hostname, user_id = self._qm.next_host()
+        if hostname is None:
+            return
 
-            if user_id:
-                print("process %s got hostname: %s" % (self._i, hostname))
+        target = self._test_server(hostname)
 
-            if hostname is None:
-                continue
+        if target[0]:
+            result_dict = dict(target=target, result=[], error=False)
 
-            target = self._test_server(hostname)
+            for command in settings.COMMAND_LIST:
+                result = self._process_command(target, command)
+                result_dict["result"].append(self._check_result(command, result))
 
-            if target[0]:
-                result_dict = dict(target=target, result=[], error=False)
+        else:
+            result_dict = target[1]
 
-                for command in settings.COMMAND_LIST:
-                    result = self._process_command(target, command)
+        if user_id:
+            self._qm.put_user_result(result_dict, user_id)
+        else:
+            self._qm.put_result(result_dict)
 
-                    if isinstance(result, PluginResult):
-                        result = result.get_raw_result()
-                        if result:
-                            result["command"] = command
-                            result["error"] = False
-                        else:
-                            result = dict(command=command, error=True, err_msg="command is not supported")
+    def _check_result(self, command, result):
+        from plugins.PluginBase import PluginResult
 
-                    elif isinstance(result, str):
-                        result = dict(command=command, error=True, err_msg=result)
-
-                    else:
-                        result = dict(command=command, error=True, err_msg="command is not supported")
-
-                    result_dict["result"].append(result)
-
+        if isinstance(result, PluginResult):
+            result = result.get_raw_result()
+            if result:
+                result["command"] = command
+                result["error"] = False
             else:
-                result_dict = target[1]
+                result = dict(command=command, error=True, err_msg="command is not supported")
 
-            if user_id:
-                self._qm.put_user_result(result_dict, user_id)
-            else:
-                self._qm.put_result(result_dict)
+        elif isinstance(result, str):
+            result = dict(command=command, error=True, err_msg=result)
+
+        else:
+            result = dict(command=command, error=True, err_msg="command is not supported")
+
+        return result
 
     def _process_command(self, target, command):
             plugin_instance = self.available_commands[command]()
@@ -114,35 +112,24 @@ def main():
 
     signal.signal(signal.SIGINT, _sigint_handler)
 
-    ##########################
-    # PLUGINS INITIALIZATION #
-    ##########################
+    # PLUGINS INITIALIZATION
     sslyze_plugins = PluginsFinder()
-    available_plugins = sslyze_plugins.get_plugins()
     available_commands = sslyze_plugins.get_commands()
 
-    ########################
-    # QUEUE INITIALIZATION #
-    ########################
-    print("connect to QueueManager ...")
-
+    # QUEUE INITIALIZATION
     c = QueueClient()
     qm = c.queue_manager()
 
-    ##########################
-    # PROCESS INITIALIZATION #
-    ##########################
-    print("starting %s sslyze worker processes ..." % settings.NUMBER_PROCESSES)
+    # PROCESS INITIALIZATION
+    while True:
 
-    for _ in xrange(settings.NUMBER_PROCESSES):
-        p = WorkerProcess(qm, available_commands, _)
+        p = WorkerProcess(qm, available_commands)
         p.start()
         process_list.append(p)
 
-    # Wait for all processes to terminate
-    for p in process_list:
+        # Wait for processes to terminate
         p.join()
-
 
 if __name__ == "__main__":
     main()
+
